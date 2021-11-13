4eeeb97c54b4e8b7ada0dedb1fe06f000cc2d415
robinesavert <robine.savert@gettonline.com>
2021-11-13 18:00:48 +0100

    added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        night: '#3D4E68'
+      },
       fontFamily: {
         'quicksand': ['"Quicksand"', 'sans-serif'],
         'sugar-cane': ['ut_sugar_caneregular']
       }
-    },
-    rotate: {
-      '-15': '-15deg'
     }
   },
   variants: {
